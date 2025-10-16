6b982292d9cdf00fc66d15822c525cf9789fdf53
Jonathas Pereira <pjonathas972@gmail.com>
2025-10-16 17:52:13 -0300

    Inform the Loguru

---
+++ logging_exemplo_erros.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.debug("Um aviso para o desenvolvedor")
+logger.info("Uma informação para o usuário")
+logger.warning("Um alerta de que algo inesperado aconteceu")
+logger.error("Um erro que impede o funcionamento de algo")  
+logger.critical("Um erro grave que pode derrubar o sistema")
