63b2af395dae21b8309eda9c6c1bbcd3c04b911e
Jonathas Pereira <pjonathas972@gmail.com>
2025-10-16 17:58:56 -0300

    alteração do exemplo.py, inserindo logger na prática

--- exemplo00.py
+++ exemplo00.py
@@ -1,14 +1,19 @@
 from loguru import logger
 
+logger.add("meu_log.log")
+
 #Trocando print por logger
 
 logger.add("meu_app.log")
 
 def soma(x, y):
-    logger.info(x)
-    logger.info(y)
-    logger.info(x + y)
-    return x + y
+    try:
+        soma = x + y
+        logger.info("soma realizada com sucesso{soma}")
+        return soma
+    except:
+        logger.critical("Erro ao somar")
 
+soma(2, "3")
 soma(2, 3)
 
